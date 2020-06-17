1f925d43133be155be3d231d1bf82f84c48ddebc
imaximciuc <maximciuc.iurii@gmail.com>
2020-06-18 00:04:31 +0300

    Homepage - desktop

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,18 @@
 $(function () {
     $(".item-slider").slick({
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        dots: true
     });
+
+    $(".search-toggle").on("click", function () {
+        $(".search-dropdown").toggleClass("shown");
+    });
+
+    $(".scroll-top").on("click", function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    })
 });
