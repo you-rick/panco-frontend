70165da4205e6ac90b0ecbc6302ab0578739cb71
imaximciuc <maximciuc.iurii@gmail.com>
2020-06-18 14:56:32 +0300

    Homepage - desktop fixes

--- app/js/main.js
+++ app/js/main.js
@@ -5,14 +5,27 @@ $(function () {
         dots: true
     });
 
-    $(".search-toggle").on("click", function () {
+    $(".search-toggle").on("click", function (e) {
+        e.preventDefault();
         $(".search-dropdown").toggleClass("shown");
     });
 
-    $(".scroll-top").on("click", function () {
+    $(".scroll-top").on("click", function (e) {
+        e.preventDefault();
         window.scrollTo({
             top: 0,
             behavior: "smooth"
         });
     })
+
+     $(window).on('scroll load', function () {
+        var topPos = $(this).scrollTop();
+        if (topPos >= 48) {
+            $('.main-header').addClass('fixed');
+            $(".topbar-navigation").appendTo("#header_nav");
+            return false;
+        }
+        $('.main-header').removeClass('fixed');
+        $(".topbar-navigation").appendTo("#topbar_nav");
+    });
 });
